ec05b2e550d2120b46ff1b609dbccadcb69ecd02
angadrazz <angadraj9534@gmail.com>
2023-02-19 11:09:07 +0530

    Add new file and modification state

--- src/App.js
+++ src/App.js
@@ -2,28 +2,9 @@
 import './App.css';
 
 const App = () => {
-  let subscriptions=[
-    {
-       id:"1",
-       date:(new Date('2021','03','23')),
-       title:"Monthly Subscription",
-       amount:"125.60"
-    },
-    {
-      id:"2",
-      date:(new Date('2020','06','28')),
-      title:"Annual Subscription",
-      amount:"1125.00"
-   },
-   {
-    id:"3",
-    date:(new Date('2021','09','05')),
-    title:"Quarterly Subscription",
-    amount:"425.50"
- }
-]
+
 return (
-  
+ <h1>Hello</h1>
 )
 }
 
